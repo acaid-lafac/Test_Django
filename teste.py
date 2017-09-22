a07272d96c41e158c72f6753eacf216ea10a69cc
Aldo <acaidp@hotmail.com>
2017-09-22 15:53:11 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,13 @@
+print("teste")
+minhaLista=[1,3,6,2,7]
+print(minhaLista)
+minhaLista.sort()
+print(minhaLista)
+meuDic={"nome":"Aldo","idade":"45"}
+print(meuDic['nome'])
+print(meuDic)
+
+def hola(nome):
+	print('hola {} {}'.format(nome, 3))
+
+hola('Aldo')
